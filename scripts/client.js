063a2a89db3af6bbb769590e2c2e0965dab8b8b8
Heather Walls <wallshkim@gmail.com>
2019-10-04 16:07:26 -0500

    This appends input values to table on DOM and then clears input fields

--- scripts/client.js
+++ scripts/client.js
@@ -17,18 +17,40 @@ function handleSubmitButton () {
     let employeeID = $('#employeeID').val();
     let employeeTitle = $('#employeeTitle').val();
     let annualSalary = $('#annualSalary').val();
-    // store in employee object
-    employeeInfo.firstName = firstName
-    employeeInfo.lastName = lastName
-    employeeInfo.employeeID = employeeID
-    employeeInfo.employeeTitle = employeeTitle
-    employeeInfo.annualSalary = annualSalary
-    // check object
-    console.log(employeeInfo);
-    // push object into employees array
-    employees.push(employeeInfo);
-    // check array
-    console.log(employees);
-    // append
-    // clear
+    // Only store if all input fields are entered
+    if(firstName && lastName && employeeID && employeeTitle && annualSalary){
+        console.log('yay all fields entered');
+        // store in employee object
+        employeeInfo.firstName = firstName
+        employeeInfo.lastName = lastName
+        employeeInfo.employeeID = employeeID
+        employeeInfo.employeeTitle = employeeTitle
+        employeeInfo.annualSalary = annualSalary
+        // check object
+        console.log(employeeInfo);
+        // push object into employees array
+        employees.push(employeeInfo);
+        // check array
+        console.log(employees);
+        // append to DOM
+        $('tbody').append(`
+            <tr>
+                <td>${firstName}</td>
+                <td>${lastName}</td>
+                <td>${employeeID}</td>
+                <td>${employeeTitle}</td>
+                <td>${annualSalary}</td>
+                <button class="deleteButton">Delete</button>
+            </tr>
+        `)
+    }// end if all fields are entered
+    else{
+        alert('Please complete all fields');
+    }// end else fields are blank
+    // clear inputs
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#employeeID').val('');
+    $('#employeeTitle').val('');
+    $('#annualSalary').val('');
 }// end handleSubmitButton
