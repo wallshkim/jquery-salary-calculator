cabc2632449a992cb68c17ad9e5a154cec134a98
Heather Walls <wallshkim@gmail.com>
2019-10-04 15:52:57 -0500

    This collects input values and stores them in an object inside the employees array

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,7 @@
 $(document).ready(readyNow);
 
+let employees = [];
+
 function readyNow(){
     console.log('in readyNow');
     $('#submitButton').on('click', handleSubmitButton);
@@ -7,5 +9,26 @@ function readyNow(){
 
 function handleSubmitButton () {
     console.log('I was clicked!');
-    
+    // create empty object to store info
+    let employeeInfo = {};
+    // collect input values
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let employeeID = $('#employeeID').val();
+    let employeeTitle = $('#employeeTitle').val();
+    let annualSalary = $('#annualSalary').val();
+    // store in employee object
+    employeeInfo.firstName = firstName
+    employeeInfo.lastName = lastName
+    employeeInfo.employeeID = employeeID
+    employeeInfo.employeeTitle = employeeTitle
+    employeeInfo.annualSalary = annualSalary
+    // check object
+    console.log(employeeInfo);
+    // push object into employees array
+    employees.push(employeeInfo);
+    // check array
+    console.log(employees);
+    // append
+    // clear
 }// end handleSubmitButton
